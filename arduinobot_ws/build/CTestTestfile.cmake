45c4dbd07bb74f8dff5f5062bf67d9aaec355505
Ahmed Elgammal <a.t.elgammal@gmail.com>
2023-10-16 22:34:12 +0300

    creating arduinobot_pub_sub package

---
+++ arduinobot_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ahmed/projects/manipulator_robot/arduinobot_ws/src
+# Build directory: /home/ahmed/projects/manipulator_robot/arduinobot_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("arduinobot_pub_sub")
