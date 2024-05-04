54e68e0c170d70a7a7f3fe068194f05fb089b897
Ahmed Elgammal <a.t.elgammal@gmail.com>
2024-05-04 21:11:50 +0300

    create the structure of arduinobot_interface

--- arduinobot_ws/src/arduinobot_controller/include/arduinobot_interface.h
+++ arduinobot_ws/src/arduinobot_controller/include/arduinobot_interface.h
@@ -1,39 +1,34 @@
-#pragma once
-
-#include "hardware_interface/joint_state_interface.h"
-#include "hardware_interface/joint_command_interface.h"
-#include "hardware_interface/robot_hw.h"
-#include "controller_manager/controller_manager.h"
-#include "ros/ros.h"
+#include <hardware_interface/robot_hw.h>
+#include <hardware_interface/joint_state_interface.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <controller_manager/controller_manager.h>
+#include <ros/ros.h>
 #include <vector>
 #include <boost/scoped_ptr.hpp>
+#pragma once
 
-class ArduinobotInterface : public hardware_interface::RobotHW
+class arduinobot_interface : public hardware_interface::RobotHW
 {
 public:
-    ArduinobotInterface(ros::NodeHandle &);
-
-    void update(const ros::TimerEvent &e);
-    void read();
-    void write(ros::Duration elapsed_time);
+    arduinobot_interface(ros::NodeHandle &); // called when timer is expired
+    void update(const ros::TimerEvent &);
+    void readFromArduino();
+    void writeToArduino(ros::Duration);
 
 private:
     ros::NodeHandle nh_;
     ros::NodeHandle pnh_;
-    ros::Duration elapsed_time_;
-    ros::Duration update_freq_;
+    ros::Duration elapsedTime_;
+    ros::Duration updateFreq_;
     ros::Timer looper_;
-    ros::Publisher hardware_pub_;
-    ros::ServiceClient hardware_srv_;
-
-    hardware_interface::JointStateInterface joint_state_interface_;
-    hardware_interface::PositionJointInterface joint_position_interface_;
-    boost::shared_ptr<controller_manager::ControllerManager> controller_manager_;
-
+    ros::Publisher hardwarePublisher_;
+    ros::ServiceClient hardwareSrv_;
+    hardware_interface::JointStateInterface jointStateInterface_;
+    hardware_interface::PositionJointInterface jointPositionInterface_;
+    boost::shared_ptr<controller_manager::ControllerManager> controllerManager_;
     std::vector<double> cmd_;
     std::vector<double> pos_;
     std::vector<double> vel_;
     std::vector<double> eff_;
-
     std::vector<std::string> names_;
 };
